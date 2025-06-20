2e2a1aa8e6ac68a4a23c8407112aff0b2bcf58c5
Hasan Mahmud Rhidoy <hasan.mahmud8177@gmail.com>
2025-06-20 23:15:10 +0800

    Update checkstyle

--- src/main/java/com/codemechanix/checkstyle_experiment/CheckstyleExperimentApplication.java
+++ src/main/java/com/codemechanix/checkstyle_experiment/CheckstyleExperimentApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CheckstyleExperimentApplication {
 
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		SpringApplication.run(CheckstyleExperimentApplication.class, args);
 	}
 
